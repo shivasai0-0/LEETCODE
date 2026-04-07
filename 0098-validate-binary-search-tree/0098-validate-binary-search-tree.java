769fb957b6d5aaeb0b878df0371400f186123484
shivasai0-0 <alladishivasai15@gmail.com>
2026-04-07 18:46:42 +0530

    Time: 0 ms (100%), Space: 44.9 MB (92.43%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,47 +15,16 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        // return check(root);
-        List<Integer> r= inorder(root);
-        int num=r.get(0);
-        for(int i=1;i<r.size();i++){
-            if(num>=r.get(i)){
-                return false;
-            }
-            num=r.get(i);
-        }
-        return true;
+        return check(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-    boolean check(TreeNode root){
+    boolean check(TreeNode root,long min, long max){
         if(root==null){
             return true;
         }
-        boolean lbool=true;
-        boolean rbool=true;
-        if(root.left!=null&&root.left.val>=root.val){
-            lbool=false;
+        if(root.val>= max || root.val <= min){
+            return false;
         }
-        if(root.right!=null&&root.right.val<=root.val){
-            rbool=false;
-        }
-        return lbool&&rbool&&check(root.right)&&check(root.left);
+        return check(root.right,root.val,max)&&check(root.left,min,root.val);
 
-    }
-    List<Integer> inorder(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            }
-            curr = stack.pop();
-            result.add(curr.val);
-
-            curr = curr.right;
-        }
-
-        return result;
     }
 }
