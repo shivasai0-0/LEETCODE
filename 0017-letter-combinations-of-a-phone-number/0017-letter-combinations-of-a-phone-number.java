0269cd03c3fede1f2c2dbd07534d5ec6a2935a26
shivasai0-0 <alladishivasai15@gmail.com>
2026-05-15 14:01:08 +0530

    Time: 1 ms (54.07%), Space: 43.3 MB (78.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,6 +1,14 @@
 class Solution {
+
     public List<String> letterCombinations(String digits) {
-        Map<Character,String> map= new HashMap<>();
+
+        List<String> sol = new ArrayList<>();
+
+        if(digits.length() == 0)
+            return sol;
+
+        Map<Character,String> map = new HashMap<>();
+
         map.put('2',"abc");
         map.put('3',"def");
         map.put('4',"ghi");
@@ -9,20 +17,32 @@ class Solution {
         map.put('7',"pqrs");
         map.put('8',"tuv");
         map.put('9',"wxyz");
-        List<String> sol= new ArrayList<>();
-        String ans="";
-        solve(sol,map,digits,digits.length(),0,ans);
+
+        solve(sol, map, digits, 0, new StringBuilder());
+
         return sol;
     }
-    public void solve(List<String> sol ,Map<Character,String> map , String digits ,int n , int index,String ans ){
-        if(index==n){
-            sol.add(ans);
+
+    public void solve(List<String> sol,
+                      Map<Character,String> map,
+                      String digits,
+                      int index,
+                      StringBuilder ans) {
+
+        if(index == digits.length()) {
+            sol.add(ans.toString());
             return;
         }
-        String alpha = map.get(digits.charAt(index));
-        for(char ch:alpha.toCharArray()){
-            solve(sol,map,digits,n,index+1,ans+ch);
-        }
 
+        String alpha = map.get(digits.charAt(index));
+
+        for(char ch : alpha.toCharArray()) {
+
+            ans.append(ch);
+
+            solve(sol, map, digits, index + 1, ans);
+
+            ans.deleteCharAt(ans.length() - 1);
+        }
     }
 }
